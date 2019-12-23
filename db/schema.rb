21b0b7f478b5b92c5de770357f83945be45f4e36
umiwatari823 <umiwatari823@gmail.com>
2019-12-23 17:02:50 +0900

    ニックネーム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_22_154524) do
+ActiveRecord::Schema.define(version: 2019_12_23_075840) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2019_12_22_154524) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
