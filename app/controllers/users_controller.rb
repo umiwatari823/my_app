934a16f4b532911dac9928e215cae775e38127f0
umiwatari823 <umiwatari823@gmail.com>
2019-12-27 20:40:50 +0900

    ユーザーマイページバグ修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
-    @nickname = current_user.nickname
-    @tweets = current_user.tweets.page(params[:page]).per(5).order("created_at desc")
+    @nickname = user.nickname
+    @tweets = user.tweets.page(params[:page]).per(5).order("created_at DESC")
   end
 end
