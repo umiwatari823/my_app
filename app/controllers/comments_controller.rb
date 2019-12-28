d853b7331d87f7e30dbea15cb025b387d2ba8a58
umiwatari823 <umiwatari823@gmail.com>
2019-12-28 16:35:57 +0900

    誤字脱字多すぎ、気をつけましょう。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,11 @@
 class CommentsController < ApplicationController
-end
-
-def create
-  Comments.create(comment_params)
-  redirect_to "/tweets/#{comment.tweet.id}"
-end
-
-private
-def comment_params
-  params.requier(:comments).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
+  def create
+    comment = Comment.create(comment_params)
+    redirect_to "/tweets/#{comment.tweet.id}"
+  end
+  
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
+  end
 end
