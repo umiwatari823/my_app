0cc435381c755d7d51b5d2e009d6d54daf15b136
umiwatari823 <umiwatari823@gmail.com>
2019-12-28 16:23:42 +0900

    アクション、ストロングパラメータ追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
 end
+
+def create
+  Comments.create(comment_params)
+  redirect_to "/tweets/#{comment.tweet.id}"
+end
+
+private
+def comment_params
+  params.requier(:comments).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
+end
