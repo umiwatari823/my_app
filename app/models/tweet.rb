75513bc10f8d2dd98440ecec3bfef214455ac444
umiwatari823 <umiwatari823@gmail.com>
2019-12-30 14:11:21 +0900

    アソシエーション追加

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,6 +2,8 @@ class Tweet < ApplicationRecord
   validates :text, presence: true
   belongs_to :user
   has_many :comments
+  has_many :likes, dependent: :destroy
+  has_many :liking_users, through: :likes, source: :user
   mount_uploader :image, ImageUploader
   def self.search(search)
     return Tweet.all unless search
