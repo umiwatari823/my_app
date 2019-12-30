916da4c26ce66580535d825ef98be9d13329b8bf
umiwatari823 <umiwatari823@gmail.com>
2019-12-30 16:20:00 +0900

    細かい修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,11 +4,19 @@ class LikesController < ApplicationController
   def like
     like = current_user.likes.new(tweet_id: @tweet.id)
     like.save
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
   end
 
   def unlike
     like = current_user.likes.find_by(tweet_id: @tweet.id)
     like.destroy
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
   end
 
   private
