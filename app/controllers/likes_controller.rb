33474498b4e1d58f12b07ae0073178348b8a8231
umiwatari823 <umiwatari823@gmail.com>
2019-12-30 14:16:43 +0900

    コントローラー記述

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,20 @@
 class LikesController < ApplicationController
+  before_action :set_variables
+
+  def like
+    like = current_user.likes.new(tweet_id: @tweet.id)
+    like.save
+  end
+
+  def unlike
+    like = current_user.likes.find_by(tweet_id: @tweet.id)
+    like.destroy
+  end
+
+  private
+
+  def set_variables
+    @tweet = Tweet.find(params[:tweet_id])
+    @id_name = "#like-link-#{@tweet.id}"
+  end
 end
