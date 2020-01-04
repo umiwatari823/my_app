dc076cdd99e1800a8a7b60a5ff512a1b5e17611f
umiwatari823 <umiwatari823@gmail.com>
2020-01-04 17:54:54 +0900

    db:reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_30_054333) do
+ActiveRecord::Schema.define(version: 2020_01_03_140122) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
