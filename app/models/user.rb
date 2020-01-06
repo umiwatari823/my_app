d5a5d3bb93e585bc262fd89ae7795d302fc0e8e3
umiwatari823 <umiwatari823@gmail.com>
2020-01-06 17:00:50 +0900

    user修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,10 @@ class User < ApplicationRecord
   has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
   has_many :followings, through: :following_relationships
 
+  has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :follower_relationships
+ 
+
   def following?(other_user)
     following_relationships.find_by(following_id: other_user.id)
   end
