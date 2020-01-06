360d96bfea75000c34d3b63d064d8c08a98439e5
umiwatari823 <umiwatari823@gmail.com>
2020-01-06 17:05:20 +0900

    コントローラー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,16 @@ class UsersController < ApplicationController
     @nickname = user.nickname
     @tweets = user.tweets.page(params[:page]).per(5).order("created_at DESC")
   end
+
+  def following
+    @user  = User.find(params[:id])
+    @users = @user.followings
+    render 'show_follow'
+  end
+
+  def followers
+    @user  = User.find(params[:id])
+    @users = @user.followers
+    render 'show_follower'
+  end
 end
