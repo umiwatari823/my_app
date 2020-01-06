b0019a5110cc5804e4131d65bc0075a639e49da3
umiwatari823 <umiwatari823@gmail.com>
2020-01-06 17:46:33 +0900

    フォロー機能完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   def show
-    user = User.find(params[:id])
-    @nickname = user.nickname
-    @tweets = user.tweets.page(params[:page]).per(5).order("created_at DESC")
+    @user = User.find(params[:id])
+    @nickname = @user.nickname
+    @tweets = @user.tweets.page(params[:page]).per(5).order("created_at DESC")
   end
 
   def following
