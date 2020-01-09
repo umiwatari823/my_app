18f0e2cc7f4ba993151e0b7a9e0df467191a5112
umiwatari823 <umiwatari823@gmail.com>
2020-01-09 19:29:49 +0900

    修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,16 +2,19 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.secrets.aws_access_key_id1,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key1,
-    region: 'ap-northeast-1'
-  }
-
-  config.fog_directory  = 'umiwatari2freema'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/umiwatari2freema'
+  CarrierWave.configure do |config|
+    if Rails.env.development? || Rails.env.test? 
+      config.storage = :file
+    elsif Rails.env.production? 
+      config.storage = :fog
+      config.fog_provider = 'fog/aws'
+      config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      region: 'ap-northeast-1'
+    }
+    config.fog_directory  = 'umiwatari2freema'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/umiwatari2freema'
+    end
 end
